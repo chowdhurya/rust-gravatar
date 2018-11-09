a054f73d702f30a4ccbd0c294e152e75c5140b10
Amit Chowdhury <amitc97@gmail.com>
2018-11-09 15:38:02 -0500

    Version 0.2 rewrite

--- tests/gravatar.rs
+++ tests/gravatar.rs
@@ -1,99 +1,104 @@
 extern crate gravatar;
-use gravatar::{Default, Rating, Gravatar};
+extern crate url;
+
+use gravatar::{Default, Gravatar, Rating};
+use url::Url;
 
 #[test]
 fn test_base_url() {
-    let g = Gravatar::new("email@example.com");
+    let url = Gravatar::new("email@example.com").image_url();
     assert_eq!(
-        g.image_url(),
+        url.as_str(),
         "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e"
     );
 }
 
 #[test]
 fn test_hash_procedure() {
-    let g = Gravatar::new("  EMaiL@exAMplE.cOm ");
+    let url = Gravatar::new("  EMaiL@exAMplE.cOm ").image_url();
     assert_eq!(
-        g.image_url(),
+        url.as_str(),
         "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e"
     );
 }
 
 #[test]
 fn test_size() {
-    let mut g = Gravatar::new("email@example.com");
-    g.size = Some(50);
+    let url = Gravatar::new("email@example.com")
+        .set_size(Some(50))
+        .image_url();
     assert_eq!(
-        g.image_url(),
-        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e\
-         ?s=50"
+        url.as_str(),
+        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e?s=50"
     );
 }
 
 #[test]
 fn test_default() {
-    let mut g = Gravatar::new("email@example.com");
-    g.default = Some(Default::Wavatar);
+    let url = Gravatar::new("email@example.com")
+        .set_default(Some(Default::Wavatar))
+        .image_url();
     assert_eq!(
-        g.image_url(),
-        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e\
-         ?d=wavatar"
+        url.as_str(),
+        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e?d=wavatar"
     );
 }
 
 #[test]
 fn test_default_url() {
     let mut g = Gravatar::new("email@example.com");
-    g.default = Some(Default::Url("http://example.org/im age".to_string()));
+    g.set_default(Some(Default::ImageUrl(
+        Url::parse("http://example.org/im age").unwrap(),
+    )));
     assert_eq!(
-        g.image_url(),
-        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e\
-         ?d=http%3A%2F%2Fexample.org%2Fim+age"
+        g.image_url().as_str(),
+        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e?d=http%3A%2F%2Fexample.org%2Fim%2520age"
     )
 }
 
 #[test]
 fn test_force_default() {
-    let mut g = Gravatar::new("email@example.com");
-    g.force_default = true;
+    let url = Gravatar::new("email@example.com")
+        .set_force_default(true)
+        .image_url();
     assert_eq!(
-        g.image_url(),
-        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e\
-         ?f=y"
+        url.as_str(),
+        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e?f=y"
     );
 }
 
 #[test]
 fn test_rating() {
-    let mut g = Gravatar::new("email@example.com");
-    g.rating = Some(Rating::R);
+    let url = Gravatar::new("email@example.com")
+        .set_rating(Some(Rating::R))
+        .image_url();
     assert_eq!(
-        g.image_url(),
-        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e\
-         ?r=r"
+        url.as_str(),
+        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e?r=r"
     );
 }
 
 #[test]
 fn test_ssl_off() {
-    let mut g = Gravatar::new("email@example.com");
-    g.ssl = false;
+    let url = Gravatar::new("email@example.com")
+        .set_ssl(false)
+        .image_url();
     assert_eq!(
-        g.image_url(),
+        url.as_str(),
         "http://www.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e"
     );
 }
 
 #[test]
 fn test_multiple_params() {
-    let mut g = Gravatar::new("email@example.com");
-    g.size = Some(200);
-    g.default = Some(Default::Identicon);
-    g.force_default = true;
-    g.rating = Some(Rating::R);
+    let url = Gravatar::new("email@example.com")
+        .set_size(Some(200))
+        .set_default(Some(Default::Identicon))
+        .set_force_default(true)
+        .set_rating(Some(Rating::R))
+        .image_url();
     assert_eq!(
-        g.image_url(),
-        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e\
-         ?s=200&d=identicon&f=y&r=r"
+        url.as_str(),
+        "https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e?s=200&d=identicon&f=y&r=r"
     );
 }
